683822a40ef5d259902568bbd90a328f142f1f60
Nikolay Yakimov <nyakimov123@gmail.com>
2024-06-21 12:31:39 +0300

    Exc-2 refactored code

--- src/sirma/Exercises/exc2/Main.java
+++ src/sirma/Exercises/exc2/Main.java
@@ -5,42 +5,54 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner scanner=new Scanner(System.in);
-        int row=Integer.parseInt(scanner.nextLine());
-        int col=Integer.parseInt(scanner.nextLine());
-        int[][] matrix1=new int[row][col];
-        int[][] matrix2= new int[row][col];
-        int[][] matrixSum=new int[row][col];
+        Scanner scanner = new Scanner(System.in);
+        int row = Integer.parseInt(scanner.nextLine());
+        int col = Integer.parseInt(scanner.nextLine());
+        int[][] matrix1 = new int[row][col];
+        int[][] matrix2 = new int[row][col];
+        int[][] matrixSum = new int[row][col];
 
-        String[] el1=scanner.nextLine().split(" ");
-        int index=0;
+        enterMatrix(row, col, matrix1, scanner);
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                matrix1[i][j]=Integer.parseInt(el1[index++]);
+        enterMatrix(row, col, matrix2, scanner);
+
+        matrixSum=getMaxMatrix(matrix1,matrix2,row,col);
+
+        printMatrix(matrixSum, row, col);
+
+    }
+
+    public static void enterMatrix(int row, int col, int[][] matrix, Scanner sc) {
+        String[] el1 = sc.nextLine().split(" ");
+        int index = 0;
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                matrix[i][j] = Integer.parseInt(el1[index++]);
             }
         }
-        String[] el2=scanner.nextLine().split(" ");
-        index=0;
+    }
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                matrix2[i][j]=Integer.parseInt(el2[index++]);
-            }
-        }
+    public static void printMatrix(int[][] matrix, int row, int col) {
 
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                matrixSum[i][j]=matrix1[i][j]+matrix2[i][j];
-            }
-        }
-
-        for(int i=0;i<row;i++){
-            for(int j=0;j<col;j++){
-                System.out.print(matrixSum[i][j]+" ");
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                System.out.print(matrix[i][j] + " ");
             }
             System.out.println();
         }
-
     }
+
+    public static int[][] getMaxMatrix(int[][] matrix1, int[][] matrix2, int row, int col) {
+        int[][] resultMatrix = new int[row][col];
+
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                resultMatrix[i][j] = matrix1[i][j] + matrix2[i][j];
+            }
+        }
+        return resultMatrix;
+    }
+
+
 }
